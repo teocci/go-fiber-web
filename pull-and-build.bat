6224f8425863cc1ccb3d5d6cbd92da1caf65a977
teocci <teocci@yandex.com>
2023-08-28 00:37:24 +0900

    update dir name

--- pull-and-build.bat
+++ pull-and-build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "project_dir=C:\path\to\your\project"  REM Replace with your project directory
+set "project_dir=D:\Apps\wb-analyzer"  REM Replace with your project directory
 set "bin_dir=%project_dir%\bin"
 set "go_exe=go.exe"
 set "git_exe=git.exe"
@@ -10,10 +10,10 @@ cd /d "%project_dir%"
 %git_exe% pull origin master
 
 echo Building the Go project...
-%go_exe% build -o %bin_dir%\YourGoExecutable.exe
+%go_exe% build -o %bin_dir%\wb-analyzer.exe
 
 echo Copying the executable to the bin directory...
-copy /y %bin_dir%\YourGoExecutable.exe %bin_dir%\
+copy /y %bin_dir%\wb-analyzer.exe %bin_dir%\
 
 echo Build process completed.
 pause
