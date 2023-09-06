10d362d3653246fc15ce2f054c56c4f9c7066a16
teocci <teocci@yandex.com>
2023-09-07 02:49:03 +0900

    Added scripts to run the wb analyzer 01

--- pull-and-build.bat
+++ pull-and-build.bat
@@ -5,6 +5,8 @@ set PROJECT_PATH=D:\code\go\%APP_NAME%
 set BIN_PATH=%PROJECT_PATH%\bin
 set VIEWS_PATH=%PROJECT_PATH%\views
 set WEB_PATH=%PROJECT_PATH%\web
+set BIN_VIEWS_PATH=%BIN_PATH%\views
+set BIN_WEB_PATH=%BIN_PATH%\web
 
 echo Pulling from Git...
 cd %PROJECT_PATH%
@@ -28,8 +30,8 @@ xcopy /y /d /i %APP_NAME%.exe %BIN_PATH%
 xcopy /y /d /i .env %BIN_PATH%
 xcopy /y /d /i config.json %BIN_PATH%
 xcopy /y /d /i run_in_background.bat %BIN_PATH%
-xcopy /y /d /i %VIEWS_PATH%\* %BIN_PATH%
-xcopy /y /d /i %WEB_PATH%\* %BIN_PATH%
+xcopy /y /d /i %VIEWS_PATH%\* %BIN_VIEWS_PATH%
+xcopy /y /d /i %WEB_PATH%\* %BIN_WEB_PATH%
 
 echo Build and copy completed.
 
